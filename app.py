4dd270385718d79384418fb9a9ae160113cba733
Nathan Bale <balenathan23@gmail.com>
2025-10-29 08:59:07 +0000

    test auto deploy pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Flask on AWS EKS!"
+    return "Auto-deploy test from GitHub Actions — it works!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
